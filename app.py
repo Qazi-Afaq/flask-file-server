2499709f9f2bc437d41ef3f56fb0f40ce4e46b7c
Afaq Qazi <afaq2qazi@gmail.com>
2023-03-02 10:14:38 -0800

    added server side code/validation for opening a specific folder

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import os
 
 
 usercontentfolders = os.path.join(os.getcwd() , 'static' , 'usercontentfolders')
-allFolders = os.listdir(usercontentfolders)
+# allFolders = os.listdir(usercontentfolders)
+allFolders = [f for f in os.listdir(usercontentfolders) if not os.path.isfile(os.path.join(usercontentfolders , f))]
+allowedMediaFilesList = ['mp4' , 'png' , 'jpg' , 'jpeg']
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    allFolders = os.listdir(usercontentfolders)
-    return render_template('home.html' , allFolders=allFolders)
+    # allFolders = os.listdir(usercontentfolders)
+    allFolders = [f for f in os.listdir(usercontentfolders) if not os.path.isfile(os.path.join(usercontentfolders , f))]
+    return render_template('home.html' , allFolders=allFolders , urlForOpeningFolders=url_for('openFolder'))
 
 @app.route('/make-folder' , methods=['POST'])
 def makeFolder():
@@ -30,17 +33,37 @@ def makeFolder():
             print("An error occured")
     return redirect(url_for('home'))
 
-@app.route("/open-folder/<foldername>")
+@app.route("/openspecificfolder/<foldername>")
 def openSpecificFolder(foldername):
-    return render_template('media.html')
+
+    try:
+        pageNo = abs(int(request.args.get('pg' , 1)))
+    except Exception:
+        pageNo = 1
+
+    currentFolderMediaPath = os.path.join(os.getcwd() , 'static' , 'usercontentfolders' , foldername)
+    start = 20 * (pageNo-1)
+    end = start + 20
+
+    currentFolderMedia = [f for f in os.listdir(currentFolderMediaPath)[start:stop] if os.path.isfile(os.path.join(currentFolderMediaPath , f)) and f.split('.')[-1].lower() in allowedMediaFilesList]
+    print(currentFolderMedia)
+
+    kwargsToSend = {
+        "folderName":foldername,
+        "media":currentFolderMedia
+    }
+    return render_template('media.html' , **kwargsToSend)
 
 
 @app.route("/open-folder")
 def openFolder():
     recievedFolderName = request.args.get('name' , None)
-    if not len(recievedFolderName.strip()) == 0 and not recievedFolderName == None:
-        print(url_for('openSpecificFolder' , foldername=recievedFolderName))
+    if not len(recievedFolderName.strip()) == 0 and not recievedFolderName == None and recievedFolderName in allFolders:
         return url_for('openSpecificFolder' , foldername=recievedFolderName)
     else:
         print('Error Cannot open Folder')
-    
+        return redirect(url_for('home'))
+
+@app.route("/home_js")
+def home_js():
+    return render_template("/js/home.js")
